7f0d99ec97de108efe8f0358e72de32d64c50d75
Tim Schendekehl <tim.dlang@t-online.de>
2026-08-02 14:48:13 +0200

    Add support for `auto[$] = ...`
    See DMD PRs https://github.com/dlang/dmd/pull/22802 and https://github.com/dlang/dmd/pull/22886.

---
+++ examples/d/testgrammard-extra/testauto1.d
@@ -0,0 +1,13 @@
+void main()
+{
+    //const[$] a1 = [1, 2];
+    auto[$] a2 = [1,2], a3=[3,4,5];
+
+    const auto[$] a4 = [1,2];
+    auto const[$] a5 = [2,2];
+}
+
+//const[$] g1 = [1, 2];
+auto[$] g2 = [1,2], g3 = [3,4,5];
+const auto[$] g4 = [1,2];
+//auto const[$] g5 = [2,2];
